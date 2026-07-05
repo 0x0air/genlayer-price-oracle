91c9a67478ac7586807abc45e075263dc68ef353
Air <xt88880@gmail.com>
2026-07-06 02:25:51 +0800

    fix: add back ['data'] nested access for CoinGecko /global endpoint

--- examples/market_overview_contract.py
+++ examples/market_overview_contract.py
@@ -24,12 +24,13 @@ class MarketOverviewContract(gl.Contract):
 
         try:
             raw = gl.eq_principle.strict_eq(fetch_global)
-            data = json.loads(raw)
-            mcap = str(data.get('total_market_cap', {}).get('usd', 'N/A'))
-            volume = str(data.get('total_volume', {}).get('usd', 'N/A'))
-            btc_dom = str(data.get('market_cap_percentage', {}).get('btc', 'N/A'))
-            coins = str(data.get('active_cryptocurrencies', 'N/A'))
-            self.summary = "Total MCap: " + mcap + " | Vol: " + volume + " | BTC: " + btc_dom + "% | Coins: " + coins
+            d = json.loads(raw)
+            data = d['data']
+            mcap = str(data['total_market_cap']['usd'])
+            volume = str(data['total_volume']['usd'])
+            btc_dom = str(data['market_cap_percentage']['btc'])
+            coins = str(data['active_cryptocurrencies'])
+            self.summary = "MCap: " + mcap + " | Vol: " + volume + " | BTC: " + btc_dom + "% | Coins: " + coins
             return self.summary
         except Exception as e:
             return "Error: " + str(e)
